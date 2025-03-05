a3c5357782c8a0c3d7413a7633efe8d44a09a0b1
camilo ramirez <camilomerce@gmail.com>
2025-03-04 22:03:29 -0500

    [Style] Format Queries

--- queries/delivery_date_difference.sql
+++ queries/delivery_date_difference.sql
@@ -8,17 +8,22 @@
 -- 2. Puedes usar la función CAST para convertir un número a un entero.
 -- 3. Puedes usar la función STRFTIME para convertir order_delivered_customer_date a una cadena, eliminando horas, minutos y segundos.
 -- 4. order_status == 'delivered' AND order_delivered_customer_date IS NOT NULL
-SELECT 
+SELECT
     c.customer_state AS State,
     CAST(
         AVG(
-            julianday(STRFTIME('%Y-%m-%d', o.order_estimated_delivery_date))
-            - julianday(STRFTIME('%Y-%m-%d', o.order_delivered_customer_date))      
-        )
-    AS INTEGER) AS Delivery_Difference
-FROM olist_orders AS o
-JOIN olist_customers AS c
-    ON o.customer_id = c.customer_id
-WHERE o.order_status = 'delivered'
-  AND o.order_delivered_customer_date IS NOT NULL
-GROUP BY c.customer_state;
+            julianday (
+                STRFTIME ('%Y-%m-%d', o.order_estimated_delivery_date)
+            ) - julianday (
+                STRFTIME ('%Y-%m-%d', o.order_delivered_customer_date)
+            )
+        ) AS INTEGER
+    ) AS Delivery_Difference
+FROM
+    olist_orders AS o
+    JOIN olist_customers AS c ON o.customer_id = c.customer_id
+WHERE
+    o.order_status = 'delivered'
+    AND o.order_delivered_customer_date IS NOT NULL
+GROUP BY
+    c.customer_state;
--- queries/top_10_least_revenue_categories.sql
+++ queries/top_10_least_revenue_categories.sql
@@ -5,17 +5,22 @@
 -- con el ingreso total de cada categoría.
 -- PISTA: Todos los pedidos deben tener un estado 'delivered' y tanto la categoría
 -- como la fecha real de entrega no deben ser nulas.
-SELECT 
-    p.product_category_name AS Category, 
+SELECT
+    p.product_category_name AS Category,
     COUNT(DISTINCT o.order_id) AS Num_order,
     SUM(py.payment_value) AS Revenue
-FROM olist_orders o
-JOIN olist_order_payments py ON o.order_id = py.order_id
-JOIN olist_order_items oi ON o.order_id = oi.order_id
-JOIN olist_products p ON oi.product_id = p.product_id
-WHERE o.order_status = 'delivered' 
+FROM
+    olist_orders o
+    JOIN olist_order_payments py ON o.order_id = py.order_id
+    JOIN olist_order_items oi ON o.order_id = oi.order_id
+    JOIN olist_products p ON oi.product_id = p.product_id
+WHERE
+    o.order_status = 'delivered'
     AND o.order_delivered_customer_date IS NOT NULL
     AND p.product_category_name IS NOT NULL
-GROUP BY p.product_category_name
-ORDER BY Revenue ASC
-LIMIT 10;
+GROUP BY
+    p.product_category_name
+ORDER BY
+    Revenue ASC
+LIMIT
+    10;
